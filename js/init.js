7431f03498ac80f473edcc4d25843313d45ae06a
michaelmessina <m2messina@k12.andoverma.us>
2018-01-16 16:52:13 -0500

    updated icons, css version, schedule

--- js/init.js
+++ js/init.js
@@ -2,7 +2,8 @@ $( document ).ready(function(){
     $('.button-collapse').sideNav({
         edge: 'left', // Choose the horizontal origin
         closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-        draggable: true // Choose whether you can drag to open on touch screens
+        draggable: true, // Choose whether you can drag to open on touch screens
+         menuWidth: 200, // Default is 300
     });
     $('.parallax').parallax();
     $('.slider').slider({
@@ -11,6 +12,6 @@ $( document ).ready(function(){
     });
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
       
-    $('.modal-trigger').leanModal();
+    $('.modal').modal();
      $('.tooltipped').tooltip({delay: 50, html:true});
 })
